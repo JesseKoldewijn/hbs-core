1b46b79773d6e4523bba30ae4e3b0f8062b5bd8d
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-04-19 12:05:09 +0200

    feat: added for loop on homepage with items that come from the ctx

--- src/routes/about/index-ctx.ts
+++ src/routes/about/index-ctx.ts
@@ -3,6 +3,5 @@ const context = {
     title: "About | Fastify Handlebars",
     desc: "This is the about page",
   },
-  description: "This is the about page",
 };
 export default context;
--- src/routes/index-ctx.ts
+++ src/routes/index-ctx.ts
@@ -1,8 +1,20 @@
+const itemLength = 10;
+
+const getItem = (id: number) => {
+  return {
+    id,
+    name: `Item name ${id}`,
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, minus.",
+  };
+};
+const items = Array.from({ length: itemLength }, (_, i) => getItem(i + 1));
+
 const context = {
   page: {
     title: "Fastify Handlebars",
     desc: "This is the home page",
   },
-  description: "This is the home page",
+  items,
 };
 export default context;
