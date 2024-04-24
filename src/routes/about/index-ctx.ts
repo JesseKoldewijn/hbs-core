728c0cba9db2829c5cafca3a053ad95b87fbab24
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-04-24 11:30:59 +0200

    fix: added missing doctype tag

--- src/routes/about/index-ctx.ts
+++ src/routes/about/index-ctx.ts
@@ -1,7 +1,10 @@
+import { items } from "~/data/items";
+
 const context = {
   page: {
     title: "About | Fastify Handlebars",
     desc: "This is the about page",
   },
+  items,
 };
 export default context;
