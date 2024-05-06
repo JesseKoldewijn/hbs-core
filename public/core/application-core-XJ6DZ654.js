ff884d45ab57f932b5935f8a9e2e327280d11ae0
JesseKoldewijn <jesse.koldewijn@denieuwezaak.nl>
2024-05-06 12:00:50 +0200

    fix: repair of js functionality

--- public/core/application-core-XJ6DZ654.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-JG7V63GM.js";var t=async()=>{(await import("./active-links-GSFUTULJ.js")).default()},i=t;export{i as default};
--- src/client/core/page-logic/active-links.ts
+++ src/client/core/page-logic/active-links.ts
@@ -1,53 +1,59 @@
 const activeLinks = async () => {
-  const setActiveLink = () => {
-    const anchors = document.querySelectorAll("a[data-visibly-active='true']");
+  document.addEventListener("DOMContentLoaded", () => {
+    const setActiveLink = () => {
+      const anchors = document.querySelectorAll(
+        "a[data-visibly-active='true']",
+      );
 
-    if (anchors.length > 0) {
-      anchors.forEach((anchor) => {
-        const currentAnchor = anchor;
-        const currentPath = window.location.pathname;
-        const currentHref = currentAnchor.getAttribute("href");
+      if (anchors.length > 0) {
+        anchors.forEach((anchor) => {
+          const currentAnchor = anchor;
+          const currentPath = window.location.pathname;
+          const currentHref = currentAnchor.getAttribute("href");
 
-        if (currentHref === currentPath) {
-          const isButton = currentAnchor.classList.contains("button");
-          if (isButton) {
-            currentAnchor.classList.add("button--active");
-          } else {
-            currentAnchor.classList.add("active");
+          if (currentHref === currentPath) {
+            const isButton = currentAnchor.classList.contains("button");
+            if (isButton) {
+              currentAnchor.classList.add("button--active");
+            } else {
+              currentAnchor.classList.add("active");
+            }
           }
+        });
+      }
+    };
+    setActiveLink();
+
+    const fhbsCtx = document.querySelector("script[data-id='fhbs-ctx']");
+
+    if (fhbsCtx) {
+      let lastRanPathname = window.location.pathname;
+      // listen for change element in the DOM
+      const observer = new MutationObserver(() => {
+        const f = document.querySelector<HTMLScriptElement>(
+          "script[data-id='fhbs-ctx']",
+        );
+        if (f) {
+          const ctx = { ...f.dataset };
+
+          if (lastRanPathname !== ctx.location) {
+            lastRanPathname = window.location.pathname;
+            return;
+          }
+
+          lastRanPathname = window.location.pathname;
+
+          setActiveLink();
         }
       });
+
+      observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+        attributes: true,
+      });
     }
-  };
-  setActiveLink();
-
-  const fhbsCtx = document.querySelector("script[data-id='fhbs-ctx']");
-
-  if (fhbsCtx) {
-    let lastRanPathname = window.location.pathname;
-    // listen for change element in the DOM
-    const observer = new MutationObserver(() => {
-      const f = document.querySelector<HTMLScriptElement>(
-        "script[data-id='fhbs-ctx']",
-      );
-      const ctx = { ...f?.dataset };
-
-      if (lastRanPathname !== ctx.location) {
-        lastRanPathname = window.location.pathname;
-        return;
-      }
-
-      lastRanPathname = window.location.pathname;
-
-      setActiveLink();
-    });
-
-    observer.observe(document.body, {
-      childList: true,
-      subtree: true,
-      attributes: true,
-    });
-  }
+  });
 };
 
 export default activeLinks;
